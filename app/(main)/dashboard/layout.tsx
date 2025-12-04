fb3bc61bfe62b1f2d1173df495390aa2865156c1
FrekiManagarm <mathchambaud@icloud.com>
2025-12-04 11:56:39 +0100

    push

--- app/(main)/dashboard/layout.tsx
+++ app/(main)/dashboard/layout.tsx
@@ -1,5 +1,13 @@
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { cookies } from "next/headers";
+
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+  const cookieStore = await cookies();
+  const sidebarOpen = cookieStore.get("sidebar_state")?.value === "true";
+
+  return (
+    <SidebarProvider defaultOpen={sidebarOpen}>{children}</SidebarProvider>
+  );
 };
 
 export default DashboardLayout;
