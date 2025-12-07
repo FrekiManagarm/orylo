a57296d4b65c0113d2014856987c231c2bbe3f1c
Mathieu Chambaud <mathchambaud2000@gmail.com>
2025-12-07 17:49:00 +0100

    push

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -46,5 +46,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 0.3,
     },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/blog/*`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
   ];
 }
